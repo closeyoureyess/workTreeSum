3a8433ef6bf6dc3a8cd75f2e02b575ac961b7d2e
mxnv <vadim.beller@mail.ru>
2024-06-06 17:13:14 +0300

    feature_treeset - add sum algorithm base on Queue(LinkedList)

---
+++ src/sumqueue/Main.java
@@ -0,0 +1,20 @@
+package sumqueue;
+
+
+public class Main {
+    public static void main(String[] args) {
+        SubTree myTree = new SubTree();
+
+        myTree.add(30);
+        myTree.add(25);
+        myTree.add(35);
+        myTree.add(15);
+        myTree.add(29);
+        myTree.add(34);
+        myTree.add(18);
+        myTree.add(40);
+        myTree.add(12);
+        myTree.sumElements();
+
+    }
+}
---
+++ src/sumqueue/MySet.java
@@ -0,0 +1,6 @@
+package sumqueue;
+
+public interface MySet {
+    void add(Integer value);
+
+}
